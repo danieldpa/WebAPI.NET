b54cbeb8b8e00380ac107a35d634426553991e67
Daniel <daniel175depaula@gmail.com>
2024-02-03 19:59:24 -0300

    Adicionar arquivos de projeto.

---
+++ DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebAPI.Models;
+
+namespace WebAPI.DataContext
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)//sempre usando para conexao com DB
+        {
+        }
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; } //Cria a tabela
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebAPI.DataContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => { //Conectando o banco 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); //builder.Configuration acesso o arquivo appsettings
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
